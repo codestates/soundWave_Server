d83fd375e55f62e75e3cdd4873cd4a6883f74f72
Yuil Chae <romesway@gmail.com>
2021-01-23 19:16:13 +0900

    [server]modify:structure

---
+++ src/entity/User.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { Group } from './Group.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  oauth: string;
+
+  @Column()
+  profile: string;
+
+  @OneToMany((type) => Group, (group) => group.user) // note: we will create author property in the Photo class below
+  groups: Group[];
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+}
