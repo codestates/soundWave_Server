e925a5ff8774ed84d734a0f4275514b4adc94a43
Hal-ang <jpjgv05@gmail.com>
2021-01-29 13:56:21 +0900

    [server] Add: groups DELETE

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Request, Response, Get, Query } from '@nestjs/common';
+import { Controller, Post, Body, Request, Response, Get, Query, Param, Delete } from '@nestjs/common';
 import { CreateGroupDto } from './dto/CreateGroupDto';
 import { GroupsService } from './groups.service';
 
@@ -31,9 +31,22 @@ export class GroupsController {
     } else if (data === "저장된 그룹이 없는 유저!") {
       res.status(406).send({message : data });
     }
-
     else {
       res.status(200).send({ data, message: "해당 유저의 그룹을 성공적으로 불러왔습니다!" })
+    }
+  }
+
+  @Delete('/delete/:id')
+  async deleteGroups(@Param('id') param, @Request() req, @Response() res) {
+    const accessToken = req.headers.authorization;
+    const groupId = Number(param);
+
+    const data = await this.groupsService.deleteGroupRequest(groupId, accessToken);
+
+    if (data === "유효하지 않은 토큰입니다!") {
+      res.status(401).send({ message : data });
+    } else if (data === "그룹 삭제 완료!") {
+      res.status(200).send({ message : data });
     }
   }
 }
