12c1a5349ac9e4a0902c3e4a36f8bb98a3aa4619
Yuil Chae <romesway@gmail.com>
2021-02-01 00:14:05 +0900

    [server]feat19/preDeploy

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,22 @@ import * as cookieParser from 'cookie-parser';
 dotenv.config();
 
 async function bootstrap() {
-  const fs = require('fs');
-  const keyFile = fs.readFileSync(__dirname + '/../../key.pem');
-  const certFile = fs.readFileSync(__dirname + '/../../cert.pem');
+  // const fs = require('fs');
+  // const keyFile = fs.readFileSync(__dirname + '/../../key.pem');
+  // const certFile = fs.readFileSync(__dirname + '/../../cert.pem');
 
   const app = await NestFactory.create(AppModule, {
-    httpsOptions: {
-      key: keyFile,
-      cert: certFile,
-    },
+    // httpsOptions: {
+    //   key: keyFile,
+    //   cert: certFile,
+    // },
   });
-  
+
   app.use(cookieParser());
 
   app.enableCors({
-    origin: ['https://localhost:3001'],
+    // origin: ['https://localhost:3001'],
+    origin: ['https:www.sounds-wave.com'],
     credentials: true,
     methods: ['GET', 'PUT', 'POST', 'PATCH', 'OPTIONS', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
